36280c59044b07a9a6e6616655ba47a8ea7b3178
Meghdad Shamsaei <meghdadshamsaei@Meghdads-MacBook-Pro.local>
2018-07-11 15:51:02 +0430

    No Change

--- Python/Collections.namedtuple.py
+++ Python/Collections.namedtuple.py
@@ -4,4 +4,4 @@ total= 0;k = input()
 fields = raw_input().strip().split()
 for i in range(k):
     total = total+int(raw_input().split()[fields.index('MARKS')])
-print float(total)/k
+print float((total)/k)
--- Python/Find the Runner-Up Score.py
+++ Python/Find the Runner-Up Score.py
@@ -6,3 +6,6 @@ if __name__ == '__main__':
     n = int(raw_input())
     arr = map(int, raw_input().split())
     print(sorted(set(arr))[-2])
+
+
+    
