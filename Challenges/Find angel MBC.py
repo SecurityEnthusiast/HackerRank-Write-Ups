b1e726d31a95b4cbea8bfbd3f363672a27651503
Meghdad Shamsaei <meghdadshamsaei@Meghdads-MacBook-Pro.local>
2018-12-31 01:21:19 +0100

    Last Day

--- Challenges/Find angel MBC.py
+++ Challenges/Find angel MBC.py
@@ -1,7 +1,7 @@
-#!/bin/python
+# Enter your code here. Read input from STDIN. Print output to STDOUT
 
 import math
 ab=float(input())
 bc=float(input())
 tetha= math.degrees(math.atan(ab/bc))
-print('{}'.format(str(int(round(tetha,1)))))
+print('{}'.format(str(int(round(tetha,0))))+"°")
